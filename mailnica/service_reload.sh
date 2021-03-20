d6ee6b67c6eef78333fe28871caf201245fbd234
endokard <45415540+endokard@users.noreply.github.com>
2021-03-20 17:34:16 +0200

    Update service_reload.sh

--- mailnica/service_reload.sh
+++ mailnica/service_reload.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cp /etc/letsencypt/live/mailnica.com/privkey.pem /etc/postfix/ssl/privkey.pem
-cp /etc/letsencypt/live/mailnica.com/cert.pem /etc/postfix/ssl/cert.pem
+cp /etc/letsencrypt/live/mailnica.com/privkey.pem /etc/postfix/ssl/privkey.pem
+cp /etc/letsencrypt/live/mailnica.com/cert.pem /etc/postfix/ssl/cert.pem
+
+cp /etc/letsencrypt/live/mailnica.com/privkey.pem /etc/nginx/ssl/privkey.pem
+cp /etc/letsencrypt/live/mailnica.com/cert.pem /etc/nginx/ssl/cert.pem
 
 systemctl reload nginx
 systemctl reload postfix
